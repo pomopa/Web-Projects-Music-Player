24774dc04d9aba73ce6e06c0ff8f1f7e0f0a8644
Pol Monne <pol.monne@students.salle.url.edu>
2025-05-13 16:43:55 +0200

    S'ha modificat el javascript per tal de que el missatge d'error i de success es mostri durant unicament 5s

--- www/app/Views/profile.php
+++ www/app/Views/profile.php
@@ -124,28 +124,5 @@
 
 
 <?= $this->section('javascript') ?>
-    <script>
-        document.getElementById('profilePicture').addEventListener('change', function(e) {
-            const file = e.target.files[0];
-            if (file) {
-                const reader = new FileReader();
-                reader.onload = function(e) {
-                    const profileContainer = document.querySelector('.rounded-circle');
-
-                    if (profileContainer.tagName === 'IMG') {
-                        profileContainer.src = e.target.result;
-                    } else {
-                        const img = document.createElement('img');
-                        img.src = e.target.result;
-                        img.classList = 'rounded-circle img-fluid border border-success';
-                        img.style = 'width: 150px; height: 150px; object-fit: cover;';
-                        img.alt = 'Profile Picture';
-
-                        profileContainer.parentNode.replaceChild(img, profileContainer);
-                    }
-                }
-                reader.readAsDataURL(file);
-            }
-        });
-    </script>
+    <script src="<?= site_url('/assets/js/profile.js') ?>"></script>
 <?= $this->endSection() ?>
---
+++ www/public/assets/js/profile.js
@@ -0,0 +1,31 @@
+document.getElementById('profilePicture').addEventListener('change', function(e) {
+    const file = e.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            const profileContainer = document.querySelector('.rounded-circle');
+
+            if (profileContainer.tagName === 'IMG') {
+                profileContainer.src = e.target.result;
+            } else {
+                const img = document.createElement('img');
+                img.src = e.target.result;
+                img.classList = 'rounded-circle img-fluid border border-success';
+                img.style = 'width: 150px; height: 150px; object-fit: cover;';
+                img.alt = 'Profile Picture';
+
+                profileContainer.parentNode.replaceChild(img, profileContainer);
+            }
+        }
+        reader.readAsDataURL(file);
+    }
+});
+
+setTimeout(() => {
+    const alerts = document.querySelectorAll('.alert');
+    alerts.forEach(alert => {
+        alert.classList.remove('show');
+        alert.classList.add('fade');
+        setTimeout(() => alert.remove(), 500);
+    });
+}, 5000);
