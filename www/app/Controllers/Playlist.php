9ea3221d02560c89c9d340becec56796c025f003
Joan Enric <joanenric.peiro@students.salle.url.edu>
2025-05-14 19:15:51 +0200

    arreglades coses de les playlist

--- www/app/Controllers/Playlist.php
+++ www/app/Controllers/Playlist.php
@@ -76,6 +76,48 @@ class Playlist extends BaseController
         }
 
         $playlist->tracks = $this->getTracks($playlist->id);
-        return view('playlist', ['playlist' => $playlist]);
+
+        $playlistId = $playlist->id;
+        $playlistName = $playlist->name ?? 'Playlist Not Found';
+        $tracks = $this->getTracks($playlist->id);
+        $playlist->tracks = $tracks;
+
+        // Utilitza la imatge del primer track, si existeix
+        $playlistImage = !empty($tracks) ? $tracks[0]->album_image : '/assets/img/default-cover.png';
+
+        $playlistCreator = $playlist->user_name ?? 'Unknown Creator';
+        $playlistCreatorId = $playlist->user_id ?? 0;
+        $creationDate = date('Y-m-d', strtotime($playlist->creationdate ?? 'now'));
+
+        $totalDuration = 0;
+        foreach ($playlist->tracks as $track) {
+            $totalDuration += $track->duration;
+        }
+
+        $formattedTotalDuration = gmdate("H:i:s", $totalDuration);
+        $tracksCount = count($playlist->tracks);
+
+        $session = session();
+        $userSession = $session->get('user');
+        $currentUserId = $userSession['id'] ?? null;
+        if($currentUserId == null) {
+            $isOwner = false;
+        } else {
+            $isOwner = $currentUserId == $playlistCreatorId;
+        }
+
+        return view('playlist', [
+            'playlist' => $playlist,
+            'playlistId' => $playlistId,
+            'playlistName' => $playlistName,
+            'playlistImage' => $playlistImage,
+            'playlistCreator' => $playlistCreator,
+            'playlistCreatorId' => $playlistCreatorId,
+            'creationDate' => $creationDate,
+            'tracks' => $playlist->tracks,
+            'formattedTotalDuration' => $formattedTotalDuration,
+            'tracksCount' => $tracksCount,
+            'isOwner' => $isOwner
+        ]);
     }
 }
--- www/app/Views/default_logged_in.php
+++ www/app/Views/default_logged_in.php
@@ -14,6 +14,8 @@
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Rounded:opsz,wght,FILL,GRAD@24,400,0,0" />
     <link id="pagestyle" href="<?= site_url('/assets/css/material-dashboard.css?v=3.1.0') ?>" rel="stylesheet" />
     <link rel="stylesheet" href="<?= site_url('/assets/css/spoty.css') ?>">
+
+    <?= $this->renderSection('styles') ?>
 </head>
 
 <body class="bg-dark">
