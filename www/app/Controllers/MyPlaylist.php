75116abaa05ec2366b1167e24210012b09765683
Pol Monne <pol.monne@students.salle.url.edu>
2025-05-15 11:54:08 +0200

    Funcions per editar les playlists fetes

--- www/app/Controllers/MyPlaylist.php
+++ www/app/Controllers/MyPlaylist.php
@@ -4,15 +4,18 @@ namespace App\Controllers;
 
 use App\Models\PlaylistModel;
 use App\Models\TrackModel;
+use App\Models\TrackPlaylistModel;
 
 class MyPlaylist extends BaseController
 {
     private TrackModel $trackModel;
     private PlaylistModel $playlistModel;
+    private TrackPlaylistModel $trackPlaylistModel;
 
     public function __construct(){
         $this->trackModel = new TrackModel();
         $this->playlistModel = new PlaylistModel();
+        $this->trackPlaylistModel = new TrackPlaylistModel();
     }
 
     public function index() {
@@ -20,7 +23,32 @@ class MyPlaylist extends BaseController
     }
 
     public function putTrack(string $trackID, int $playlistID) {
+        //TODO Afegir la canço a la BBDD si no existeix
+        if ($this->trackPlaylistModel->where('playlist_id', $playlistID)->where('track_id', $trackID)->first()) {
+            return $this->response->setStatusCode(404)->setJSON([
+                'status'  => 'error',
+                'message' => 'A playlist with this track already exist in the system.'
+            ]);
+        }
 
+        if (!$this->trackModel->find($trackID) || !$this->playlistModel->find($playlistID)) {
+            return $this->response->setStatusCode(404)->setJSON([
+                'status'  => 'error',
+                'message' => 'The provided playlist or track ID does not exist in the system.'
+            ]);
+        }
+
+        if (!$this->trackPlaylistModel->insert(['playlist_id' => $playlistID, 'track_id' => $trackID])) {
+            return $this->response->setStatusCode(404)->setJSON([
+                'status'  => 'error',
+                'message' => 'The track could not be added to the playlist.'
+            ]);
+        }
+
+        return $this->response->setStatusCode(200)->setJSON([
+            'status'  => 'success',
+            'message' => 'Track added successfully from the playlist.'
+        ]);
     }
 
     public function putPlaylist(int $playlistID) {
@@ -28,28 +56,24 @@ class MyPlaylist extends BaseController
     }
 
     public function deleteTrack(string $trackID, int $playlistID) {
-        $track = $this->trackModel
-            ->where('id', $trackID)
-            ->where('playlist_id', $playlistID);
-
-        if (!$track || !$this->playlistModel->find($playlistID)) {
+        if (!$this->trackPlaylistModel->where('playlist_id', $playlistID)->where('track_id', $trackID)->first()) {
             return $this->response->setStatusCode(404)->setJSON([
                 'status'  => 'error',
-                'message' => 'Playlist or track does not exist in the system.'
+                'message' => 'A playlist with this track does not exist in the system.'
             ]);
         }
 
-        if (!$this->trackModel->delete($trackID)) {
+        if (!$this->trackPlaylistModel->where('playlist_id', $playlistID)->where('track_id', $trackID)->delete()) {
             return $this->response->setStatusCode(400)->setJSON([
                 'status'  => 'error',
-                'message' => 'The track could not be deleted.',
+                'message' => 'The track could not be deleted from the playlist.',
                 'errors'  => $this->trackModel->errors()
             ]);
         }
 
         return $this->response->setStatusCode(200)->setJSON([
             'status'  => 'success',
-            'message' => 'Track deleted successfully.'
+            'message' => 'Track deleted successfully from the playlist.'
         ]);
     }
 
