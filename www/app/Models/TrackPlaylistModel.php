f9ab9992f917ccfa4b96d9c3b8d270096cbec1be
Pol Monne <pol.monne@students.salle.url.edu>
2025-05-15 11:32:07 +0200

    Models actualitzats amb les noves migracions i taules

---
+++ www/app/Models/TrackPlaylistModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use CodeIgniter\Model;
+
+class TrackPlaylistModel extends Model
+{
+    protected $table            = 'track_playlist';
+    protected $primaryKey       = null;
+    protected $returnType       = 'array';
+    protected $allowedFields    = [
+        'track_id',
+        'playlist_id',
+        'created_at',
+    ];
+
+    protected $useTimestamps = true;
+    protected $createdField  = 'created_at';
+
+    protected $validationRules    = [
+        'track_id'    => 'required|alpha_numeric|max_length[255]',
+        'playlist_id' => 'required|is_natural_no_zero',
+    ];
+
+    protected $validationMessages = [
+        'track_id' => [
+            'required'    => 'Track ID is required.',
+            'alpha_numeric' => 'Track ID must be alphanumeric.',
+            'max_length'  => 'Track ID must be at most 255 characters.',
+        ],
+        'playlist_id' => [
+            'required'           => 'Playlist ID is required.',
+            'is_natural_no_zero' => 'Playlist ID must be a positive integer.',
+        ],
+    ];
+}
